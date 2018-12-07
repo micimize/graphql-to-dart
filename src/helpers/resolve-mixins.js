422caf302f7e9089071298735efa940fb9cf6a75
micimize <rosenthalm93@gmail.com>
2018-12-07 13:14:18 -0600

    old fixes

--- src/helpers/resolve-mixins.js
+++ src/helpers/resolve-mixins.js
@@ -2,12 +2,14 @@ export default function resolveMixins(
   mixins = [],
   fields = []
 ) {
-  return mixins.filter(mixin => {
-    for (let { name } of fields){
-      if (mixin.when.fields.includes(name)){
-        return true
+  let fieldNames = fields.map(f => f.name);
+  return mixins.filter(({ when = { fields: [] } }) => {
+    for (let requiredField of when.fields){
+      if (!fieldNames.includes(requiredField)){
+        return false
       }
     }
+    return true
   }).map(mixin => mixin.name)
 }
 
